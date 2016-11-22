a241c2c8f04fa0ffaba65b0f27304687e908da67
gadsrini <gad.srini@gmail.com>
2016-11-22 20:49:37 +0530

    updateing

--- src/main/java/org/pkrm/restfullService/messanger/MyResource.java
+++ src/main/java/org/pkrm/restfullService/messanger/MyResource.java
@@ -20,6 +20,12 @@ public class MyResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
-        return "Got it!";
+        String str = "node {\n"+ 
+    		  " stage 'Stage 1'\n"+
+    		  " echo 'Hello World 1'\n"+
+    		  " stage 'Stage 2'\n"+
+    		  " echo 'Hello World 2'\n"+
+    		"}\n";
+        return str;
     }
 }
